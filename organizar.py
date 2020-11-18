c256b40f2f5dfb0bcc4bb2ff4ab0878b1664d259
Paulo Rodrigues <prodrigues1912@gmail.com>
2020-11-18 02:38:08 -0300

    ajuste final

--- organizar.py
+++ organizar.py
@@ -2,10 +2,11 @@ import os
 import shutil
 
 import regex as re
+
 import funcoes as fun
 
-pastas = os.listdir('BACKUP SEF II 09.2012 A 09.2019')
-print (pastas)
+pastas = os.listdir("BACKUP SEF II 09.2012 A 09.2019")
+print(pastas)
 
 for pasta in pastas:
     pasta_processar = "BACKUP SEF II 09.2012 A 09.2019/" + pasta
@@ -14,7 +15,7 @@ for pasta in pastas:
 
     try:
         os.mkdir(pasta_processar + "/data")
-        print('pasta /data criada em ' + pasta_processar)
+        print("pasta /data criada em " + pasta_processar)
     except FileExistsError:
         # print('pasta data/ já existe em ' + pasta_processar)
         pass
@@ -29,7 +30,7 @@ for pasta in pastas:
             for f in os.listdir(pasta_processar)
             if os.path.isfile(os.path.join(pasta_processar, f))
         ]
-        pasta_atual = pasta_processar + '/data/' + str(contador)
+        pasta_atual = pasta_processar + "/data/" + str(contador)
         try:
             os.mkdir(pasta_atual)
         except FileExistsError:
@@ -37,29 +38,40 @@ for pasta in pastas:
 
         for f in files:
             if not f.endswith(".xml") and not f.endswith(".zip"):
-                qtd_arquivos_pasta = fun.qtd_arquivos(pasta_atual) 
+                qtd_arquivos_pasta = fun.qtd_arquivos(pasta_atual)
 
                 if qtd_arquivos_pasta < qtd_max_pasta:
-                    shutil.copyfile(os.path.join(pasta_processar,f), os.path.join(pasta_atual,f))
+                    shutil.copyfile(
+                        os.path.join(pasta_processar, f), os.path.join(pasta_atual, f)
+                    )
                 elif qtd_arquivos_pasta == qtd_max_pasta:
-                    with open(pasta_atual + "/INDICE.xml",'w', encoding="ISO-8859-1") as indice:
-                        indice.write(xml_header[0] + "".join(xml_items[0:qtd_arquivos_pasta]) + xml_final)
+                    with open(
+                        pasta_atual + "/INDICE.xml", "w", encoding="ISO-8859-1"
+                    ) as indice:
+                        indice.write(
+                            xml_header[0]
+                            + "".join(xml_items[0:qtd_arquivos_pasta])
+                            + xml_final
+                        )
                     xml_items = xml_items[qtd_arquivos_pasta:]
 
                     contador = contador + 1
-                    
-                    pasta_atual = pasta_processar + '/data/' + str(contador)
-                    
+
+                    pasta_atual = pasta_processar + "/data/" + str(contador)
+
                     try:
                         os.mkdir(pasta_atual)
                     except FileExistsError:
                         pass
-                    
-                    shutil.copyfile(os.path.join(pasta_processar,f), os.path.join(pasta_atual,f))
-                    
-        with open(pasta_atual + "/INDICE.xml",'w', encoding="ISO-8859-1") as indice:
-            indice.write(xml_header[0] + "".join(xml_items[0:fun.qtd_arquivos(pasta_atual)]) + xml_final)
-        xml_items = xml_items[fun.qtd_arquivos(pasta_atual):]
 
+                    shutil.copyfile(
+                        os.path.join(pasta_processar, f), os.path.join(pasta_atual, f)
+                    )
 
-
+        with open(pasta_atual + "/INDICE.xml", "w", encoding="ISO-8859-1") as indice:
+            indice.write(
+                xml_header[0]
+                + "".join(xml_items[0 : fun.qtd_arquivos(pasta_atual)])
+                + xml_final
+            )
+        xml_items = xml_items[fun.qtd_arquivos(pasta_atual) :]
--- zippar.py
+++ zippar.py
@@ -1,17 +1,50 @@
 import os
 import zipfile
 
-pastas = os.listdir('BACKUP SEF II 09.2012 A 09.2019')
-print (pastas)
+root = "BACKUP SEF II 09.2012 A 09.2019"
+
+pastas = os.listdir(root)
+# print (pastas)
 
 for pasta in pastas:
-    subpastas = os.listdir(pasta)
+    subpastas = [
+        f
+        for f in os.listdir(os.path.join(root, pasta))
+        if not os.path.isfile(os.path.join(root, pasta, f))
+    ]  # os.listdir(os.path.join(root, pasta))
+    # print(subpastas)
+    for subpasta in subpastas:
+        pastas_data = [
+            f
+            for f in os.listdir(os.path.join(root, pasta, subpasta))
+            if not os.path.isfile(os.path.join(root, pasta, subpasta, f))
+        ]  # os.listdir(os.path.join(root, pasta, subpasta))
+        # print(pastas_data)
+        for pasta_data in pastas_data:
+            pastas_zip = [
+                f
+                for f in os.listdir(os.path.join(root, pasta, subpasta, pasta_data))
+                if os.path.isfile(os.path.join(root, pasta, subpasta, pasta_data, f))
+            ]
+            # print(pastas_zip)
+            for file in pastas_zip:
+                zip_teste = zipfile.ZipFile(
+                    "zips_para_importar/"
+                    + root
+                    + "_"
+                    + pasta
+                    + "_"
+                    + subpasta
+                    + "_"
+                    + pasta_data
+                    + ".zip",
+                    "a",
+                )
+                print(file)
+                zip_teste.write(
+                    os.path.join(root, pasta, subpasta, pasta_data, file),
+                    file,
+                    compress_type=zipfile.ZIP_DEFLATED,
+                )
 
-
-# zip_teste = zipfile.ZipFile('zips_para_importar/' + , 'w')
- 
-# for file in os.listdir('BACKUP SEF II 09.2012 A 09.2019/01/data/1'):
-#     print(file)
-#     zip_teste.write(os.path.join('BACKUP SEF II 09.2012 A 09.2019/01/data/1', file),  file, compress_type = zipfile.ZIP_DEFLATED)
- 
-# zip_teste.close()
+                zip_teste.close()
